5fd3f46d5be2f64d1dfcd09af453ae7992e32f44
Conal Crummey <conalcrummey@conals-air.home>
2023-03-22 19:53:24 +0000

    added barebones method for findNewPosition() -Still a WIP.

--- SaveOurPlanetGameSystem/src/game/Board.java
+++ SaveOurPlanetGameSystem/src/game/Board.java
@@ -26,4 +26,53 @@ public class Board {
 	private void build() {
 
 	}
+	
+	
+	/**
+	 *  Method for finding new user position.
+	 */
+	public static void findNewPosition(int playerPosition, int numberOfMoves) {
+		
+		// getting the current player
+		Player currentPlayer = getCurrentPlayer(); 
+		
+		// Find the player's new position
+		int newPlayerPosition = playerPosition+numberOfMoves;
+		
+
+		// Will need further logic here to set the player's new position (in Player Class Position int) 
+		
+
+		// Find the Square Type based on the new player position 
+		String squareType = findSquareType(newPlayerPosition);
+		
+		
+		if (squareType == "Tax Square Type") {
+			TaxSquare.activateEvent(currentPlayer);
+		} 
+		else if (squareType == "Donation Square Type") {
+			DonationSquare.activateEvent(currentPlayer);
+		} 
+		else {
+		System.out.println("Move Complete");
+		
+		}
+
+	}
+
+	public static Player getCurrentPlayer() {
+
+		// finds the current player and returns them
+		Player dummyPlayer = "Placeholder Player 1";
+		return dummyPlayer;
+
+	}
+
+	public static String findSquareType(int index) {
+
+		// finds the new square that the user lands on and returns the square type
+		String dummySquareType = "Tax Square";
+		return dummySquareType;
+
+	}
 }
